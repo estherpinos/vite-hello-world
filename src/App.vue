622db84a3792bacda95881fc583c8af63656ee06
estherpinos <estherpinos1@gmail.com>
2023-10-09 17:57:03 +0200

    Add [files]

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+ 
+<script>
+
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+
+  export default {
+    name: 'App',
+    components:{
+      AppTitle,
+      AppHero
+     
+    }
+  }
+</script>
+
+<template>
+  <AppTitle></AppTitle> 
+  <AppHero></AppHero>
+
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<script>
+
+  export default {
+    name: 'AppHero'
+  }
+</script>
+
+<template>
+  <div class="jumbotron">
+    
+  </div>
+
+</template>
+
+<style>
+
+  .jumbotron{
+    height: 300px;
+    width: 100%;
+    background-color: aquamarine;
+  }
+</style>
